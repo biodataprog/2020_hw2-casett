1e9c61c71089125b6280b3fb52fce938901adbb1
Cassie <clettinger@ucdavis.edu>
2020-11-06 20:20:22 -0800

    updated codon_compute.py

--- codon_compute.py
+++ codon_compute.py
@@ -22,6 +22,8 @@ url1="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_coll
 url2="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/mycobacterium_tuberculosis_h37rv/cds/Mycobacterium_tuberculosis_h37rv.ASM19595v2.cds.all.fa.gz"
 file1="Salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2.ASM694v2.cds.all.fa.gz"
 file2="Mycobacterium_tuberculosis_h37rv.ASM19595v2.cds.all.fa.gz"
+sp1 = "S. enterica"
+sp2 = "M. tuberculosis"
 
 if not os.path.exists(file1):
     os.system("curl -O %s"%(url1))
@@ -31,8 +33,99 @@ if not os.path.exists(file2):
 
 with gzip.open(file1,"rt") as fh:
     seqs = aspairs(fh)
-
+    
+    num_genes = 0
+    total_len = 0
+    gc = 0
+    codon_count = {}
+    num_codons = 0
+    
     for seq in seqs:
         seqname  = seq[0]
         seqstring= seq[1]
-        print(seqname, " first 10 bases are ", seqstring[0:10])
+        
+        num_genes += 1
+        total_len += len(seqstring)
+        
+        #codon counting probably needs to be its own helper function
+        #but a problem for another day
+        codon = ""
+        for base in seqstring:
+            codon += base
+            
+            if base in ["G", "C"]:
+                gc += 1
+            
+            if len(codon) == 3:
+                if codon in codon_count:
+                    codon_count[codon] += 1
+                else:
+                    codon_count[codon] = 1
+                        
+                codon = ""
+    
+        
+    for codon in codon_count:
+        num_codons += codon_count[codon]
+        #print(codon, codon_count[codon]) 
+
+    print("The total number of genes for", sp1, ":", num_genes)
+    print("The total genome length for", sp1, ":",total_len)
+    print("The % GC content for", sp1, "is:", gc/total_len*100)
+    print("The total number of codons for", sp1, ":", num_codons)
+
+
+with gzip.open(file2,"rt") as fh:
+    seqs = aspairs(fh)
+    
+    num_genes = 0
+    total_len = 0
+    gc = 0
+    codon_count_sp2 = {}
+    num_codons = 0
+    
+    for seq in seqs:
+        seqname  = seq[0]
+        seqstring= seq[1]
+        
+        num_genes += 1
+        total_len += len(seqstring)
+        
+        
+        codon = ""
+        for base in seqstring:
+            codon += base
+            
+            if base in ["G", "C"]:
+                gc += 1
+            
+            if len(codon) == 3:
+                if codon in codon_count_sp2:
+                    codon_count_sp2[codon] += 1
+                else:
+                    codon_count_sp2[codon] = 1
+                        
+                codon = ""
+    
+        
+    for codon in codon_count_sp2:
+        num_codons += codon_count_sp2[codon]
+        #print(codon, codon_count[codon]) 
+
+    print("The total number of genes for", sp2, ":", num_genes)
+    print("The total genome length for", sp2, ":",total_len)
+    print("The % GC content for", sp2, "is:", gc/total_len*100)
+    print("The total number of codons for", sp2, ":", num_codons)
+
+
+print("Codon \t", sp1, "\t", sp2)
+for codon in codon_count:
+    if codon in codon_count_sp2:
+        print(codon, "\t", codon_count[codon], "\t", codon_count_sp2[codon])
+    else:
+        print(codon, "\t", codon_count[codon], "\t", 0)
+for codon in codon_count_sp2:
+    if codon not in codon_count:
+        print(codon, "\t", 0, "\t", codon_count_sp2[codon])
+        
+        
